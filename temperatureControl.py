d07b39f21f80c5653b31c60e172ebf8ca33a7edd
Travis Swanson <travis.swanson@gmail.com>
2022-08-07 06:59:27 -0400

    Simultaneous operation of Ardunio API and DallasTemperature

--- temperatureControl.py
+++ temperatureControl.py
@@ -1,25 +1,51 @@
 from nanpy import DallasTemperature
+from nanpy import (ArduinoApi, SerialManager)
+import serial
+from nanpy.arduinoboard import arduinomethod
+from nanpy.classinfo import check4firmware
 import time
+from datetime import datetime
 
+connection = SerialManager()
+sensors = DallasTemperature(2, 'COM3', connection)
 
+a = ArduinoApi(connection=connection)
 
-sensors = DallasTemperature(2,'COM3')
 n_sensors = sensors.getDeviceCount()
+sensors.setResolution(12)
+
+a.pinMode(7, a.OUTPUT)
 
 print("There are %d devices connected on pin %d" % (n_sensors, sensors.pin))
+
 addresses = []
 
 for i in range(n_sensors):
     addresses.append(sensors.getAddress(i))
 
-sensors.setResolution(12)
+onOrOff = 0
+file1 = open('temperatureRecord.txt','a')
 
-while True:
+for i in range(int(12*60*(60/4))):
+    t = time.time()
     sensors.requestTemperatures()
     for i in range(n_sensors):
         temp = sensors.getTempC(i)
         print("Device %d (%s) temperature, in Celsius degrees is %0.2f" % (i, addresses[i], temp))
         print("Let's convert it in Fahrenheit degrees: %0.2f" % DallasTemperature.toFahrenheit(temp))
     print("\n")
-    time.sleep(2)
 
+    a.digitalWrite(7, (onOrOff))
+    if onOrOff:
+        onOrOff = 0
+    else:
+        onOrOff = 1
+
+    now = datetime.now()
+    currentTime = now.strftime("%d/%m/%y %H:%M:%S")
+    L = str(i) + ',' + currentTime + ',' + str(sensors.getTempC(0)) + ',' + str(sensors.getTempC(1))
+    file1.write(L + ' \n')
+
+    time.sleep(15)
+
+file1.close()
